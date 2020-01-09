4db247c895d42b5df1c90082d6f56388fd9aa410
Denali Balser <denali.balser167@gmail.com>
2020-01-09 16:58:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,6 +81,16 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL 
+    SELECT * 
+    FROM students 
+    WHERE students.grade = ? 
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end 
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
